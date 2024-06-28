ae5b9c88573577de508ace1ec896f3a48a2d3b90
brunozanao <brunovinicius_2012@hotmail.com>
2024-06-28 12:53:04 -0300

    commit 13 dia 28

--- src/pages/half-slider.js
+++ src/pages/half-slider.js
@@ -4,9 +4,19 @@ import BlogComponent from "@components/BlogComponent";
 import Drops from "@components/Drops";
 import ServicesComponent from "@components/ServicesComponent";
 import WhyChooseUsComponent from "@components/WhyChooseUsComponent";
+// Import Swiper React components
+import React from 'react';
 import { Swiper, SwiperSlide } from 'swiper/react';
-import 'swiper/swiper-bundle.min.css';
+
+// Import Swiper styles
+import 'swiper/swiper.min.css'; // ou 'swiper/swiper-bundle.min.css'
+import 'swiper/components/effect-creative/effect-creative.min.css'; // se você estiver usando EffectCreative
+import 'swiper/components/autoplay/autoplay.min.css'; // se você estiver usando Autoplay
+
+// Import Swiper core and required modules
 import SwiperCore, { Autoplay, EffectCreative } from 'swiper';
+
+// Install modules
 SwiperCore.use([Autoplay, EffectCreative]);
 import React, { useContext } from 'react';
 
