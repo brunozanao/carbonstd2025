74a1375b37c34f064872f136860307370beaeb35
brunozanao <brunovinicius_2012@hotmail.com>
2024-06-28 13:04:21 -0300

    commit 18 dia 28

--- src/pages/half-slider.js
+++ src/pages/half-slider.js
@@ -1,3 +1,7 @@
+import Layout from "@/layout/Layout";
+import AboutComponent from "@components/AboutComponent";
+import BlogComponent from "@components/BlogComponent";
+import Drops from "@components/Drops";
 import React from 'react'; // Import React uma vez
 
 // Import Swiper React components
@@ -19,26 +23,90 @@ import WhyChooseUsComponent from "@components/WhyChooseUsComponent";
 
 const HalfSlider = () => {
   return (
-    <Swiper
-      spaceBetween={50}
-      slidesPerView={1}
-      autoplay={{ delay: 2500 }}
-      effect="creative"
-      creativeEffect={{
-        prev: {
-          shadow: true,
-          translate: [0, 0, -400],
-        },
-        next: {
-          translate: ['100%', 0, 0],
-        },
-      }}
-    >
-      <SwiperSlide>Slide 1</SwiperSlide>
-      <SwiperSlide>Slide 2</SwiperSlide>
-      <SwiperSlide>Slide 3</SwiperSlide>
-    </Swiper>
+    <Layout pageName={"Home Half Slider"}>
+      <div className="neoh_fn_hero half">
+        {/* Overlay (of hero header) */}
+        <div className="bg_overlay">
+          {/* Overlay Slider */}
+          <div className="overlay_slider">
+            <Swiper
+              loop={true}
+              effect={"creative"}
+              autoplay={{
+                delay: 5000,
+                disableOnInteraction: false,
+              }}
+              creativeEffect={{
+                prev: {
+                  shadow: true,
+                  translate: ["-20%", 0, -1],
+                },
+                next: {
+                  translate: ["100%", 0, 0],
+                },
+              }}
+              modules={[EffectCreative, Autoplay]}
+              className="swiper-container"
+            >
+              <div className="swiper-wrapper">
+                <SwiperSlide className="swiper-slide">
+                  <div className="main_image" data-bg-img="img/drops/1.jpg" />
+                </SwiperSlide>
+                <SwiperSlide className="swiper-slide">
+                  <div className="main_image" data-bg-img="img/drops/2.jpg" />
+                </SwiperSlide>
+                <SwiperSlide className="swiper-slide">
+                  <div className="main_image" data-bg-img="img/drops/3.jpg" />
+                </SwiperSlide>
+                <SwiperSlide className="swiper-slide">
+                  <div className="main_image" data-bg-img="img/drops/4.jpg" />
+                </SwiperSlide>
+                <SwiperSlide className="swiper-slide">
+                  <div className="main_image" data-bg-img="img/drops/5.jpg" />
+                </SwiperSlide>
+              </div>
+            </Swiper>
+          </div>
+          {/* !Overlay Slider */}
+        </div>
+        {/* Overlay (of hero header) */}
+        <div className="hero_content">
+          <div className="container">
+            <div className="content">
+              <h2 className="fn_title" title="Neoh">
+                Neoh
+              </h2>
+              <p className="fn_desc fn_animated_text">
+                The collection built by and runby ‘codeefly’. Together we are
+                strong than ever.
+              </p>
+            </div>
+          </div>
+          <a
+            href="#about"
+            className="neoh_fn_down magic-hover magic-hover__square"
+          >
+            <span className="text">Scroll Down</span>
+            <span className="icon">
+              <img src="svg/right-arr.svg" alt="" className="fn__svg" />
+            </span>
+          </a>
+        </div>
+      </div>
+      {/* !Hero Header */}
+      {/* About Section */}
+      <AboutComponent />
+      {/* !About Section */}
+      {/* Services Section */}
+      <ServicesComponent />
+      {/* !Services Section */}
+      <Drops />
+      {/* Investor Section */}
+      <WhyChooseUsComponent />
+      {/* !Investor Section */}
+      {/* Blog Section */}
+      <BlogComponent />
+    </Layout>
   );
 };
-
 export default HalfSlider;
