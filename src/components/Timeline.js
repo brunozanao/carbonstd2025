d9056d3eac85cc2c6e55eba67732ef9adb98e3dc
brunozanao <brunovinicius_2012@hotmail.com>
2024-09-05 14:22:58 -0300

    testando time

--- src/components/Timeline.js
+++ src/components/Timeline.js
@@ -314,7 +314,7 @@ const Timeline = () => {
             className="nav_prev c-pointer"
             onClick={() =>
               setActiveTimeline(
-                activeTimeline == 1 ? activeTimeline : activeTimeline - 1
+                activeTimeline == 0 ? activeTimeline : activeTimeline - 1
               )
             }
           >
