18a4a5eace8fbb13cc53355b9d18b0e64fea2f3e
brunozanao <brunovinicius_2012@hotmail.com>
2024-09-12 11:59:11 -0300

    button roxo

--- src/components/AboutComponent.js
+++ src/components/AboutComponent.js
@@ -77,7 +77,11 @@ const AboutComponent = () => {
               </p>
             </div>
             <div className="buttons">
-              <link legacyBehavior><a target="_blank" href="https://carbonstd.gitbook.io/carbon.std" class="neoh_fn_button only_text"><span class="text">More About US</span></a></link>
+              <a target="_blank" legacyBehavior href="https://carbonstd.gitbook.io/carbon.std">
+                <a className="neoh_fn_button only_text">
+                  <span className="text">More About US</span>
+                </a>
+              </a>
             </div>
           </div>
         </div><br></br><br></br>
